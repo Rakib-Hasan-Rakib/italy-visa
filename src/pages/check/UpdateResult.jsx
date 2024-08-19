b38cf5f6ba2f1dfa6bd6c024bab89d76cd310ba3
Rakib-Hasan-Rakib <rakibhasanrakib3006@gmail.com>
2024-08-19 22:41:40 +0600

    x

--- src/pages/check/UpdateResult.jsx
+++ src/pages/check/UpdateResult.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import toast from "react-hot-toast";
 import { ImSpinner4 } from "react-icons/im";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const UpdateResult = ({ passNum }) => {
   const [loading, setLoading] = useState(false);
+  const { user } = useContext(AuthContext);
   const handleFormSubmit = async (e) => {
     e.preventDefault();
 
@@ -42,26 +44,38 @@ const UpdateResult = ({ passNum }) => {
 
   return (
     <>
-      <form onSubmit={handleFormSubmit}>
-        <h3 className="gradient-text text-xl md:text-2xl lg:text-3xl text-center font-semibold mt-5">
-          Add more files
-        </h3>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-          <input type="file" name="pdf9" accept=".pdf" className="input-box" />
-          <input type="file" name="pdf10" accept=".pdf" className="input-box" />
-        </div>
-        <div className="flex justify-center items-center">
-          {loading ? (
-            <button className="submit-btn">
-              <ImSpinner4 size={24} className="animate-spin" />
-            </button>
-          ) : (
-            <button className="submit-btn">
-              <input type="submit" value="Add Now" />
-            </button>
-          )}
-        </div>
-      </form>
+      {user?.email == "haquemdnurul108@gmail.com" && (
+        <form onSubmit={handleFormSubmit}>
+          <h3 className="gradient-text text-xl md:text-2xl lg:text-3xl text-center font-semibold mt-5">
+            Add more files
+          </h3>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <input
+              type="file"
+              name="pdf9"
+              accept=".pdf"
+              className="input-box"
+            />
+            <input
+              type="file"
+              name="pdf10"
+              accept=".pdf"
+              className="input-box"
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            {loading ? (
+              <button className="submit-btn">
+                <ImSpinner4 size={24} className="animate-spin" />
+              </button>
+            ) : (
+              <button className="submit-btn">
+                <input type="submit" value="Add Now" />
+              </button>
+            )}
+          </div>
+        </form>
+      )}
     </>
   );
 };
