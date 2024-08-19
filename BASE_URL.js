9c54d2594f07e2318ba2fbf0979e5300156af0b3
Rakib-Hasan-Rakib <rakibhasanrakib3006@gmail.com>
2024-08-19 20:41:36 +0600

    x

--- BASE_URL.js
+++ BASE_URL.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3000";
-// export const BASE_URL = "https://italy-visa-server.onrender.com"
+// export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://italy-visa-server.onrender.com"
